458781b47034a9258c51c462dc12009fa0a88d0f
ikido <ikidoit@gmail.com>
2013-04-17 14:23:52 +0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happymarketer::Application.config.secret_token = '39ec01d08ab3b37292e5b2fa2d31cdd89edc1a6e14378098449c0cc3221f42bcee383cffee573c2591835a4cd8f45272e354841ead9ee53b0431605593c0bab2'
